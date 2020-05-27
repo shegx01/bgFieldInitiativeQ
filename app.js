42ef00c2d92a64bbe5bd506765d242d1506677ba
O.O Samson <shegx01@gmail.com>
2020-05-27 09:45:42 +0100

    navigation menu and html overhaul

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const mainNav = document.querySelector('#mainNav')
 const stickyNav = document.querySelector('#stickyNav')
 const learnMoreBtnOne = document.querySelector('#learnMoreOne')
 const learnMoreBtnTwo = document.querySelector('#learnMoreTwo')
+const tooltip = document.querySelector('.tooltip-one')
+
+// hooks for mobile menu
+const mobileBtnOne = document.querySelector('#mobileBtnOne')
+const mobileBtnTwo = document.querySelector('#mobileBtnTwo')
+const mobileMenu = document.querySelector('#mobileMenu')
+const mobileMenuClose = document.querySelector('#mobileClose')
 
 const changePos = () => {
     if (window.pageYOffset > 70) {
@@ -24,13 +31,25 @@ const eventListeners = () => {
     window.onscroll = changePos;
 
     learnMoreBtnOne.addEventListener('click', () => {
-       const tooltip = document.querySelector('.tooltip-one')
-       tooltip.classList.toggle('showModal')
+        tooltip.classList.toggle('showModal')
     })
     learnMoreBtnTwo.addEventListener('click', () => {
-       const tooltip = document.querySelector('.tooltip-two')
-       tooltip.classList.toggle('showModal')
+        const tooltip = document.querySelector('.tooltip-two')
+        tooltip.classList.toggle('showModal')
     })
+    mobileBtnOne.addEventListener('click', () => {
+        mobileMenu.classList.add('show-mobile')
+    })
+    mobileBtnTwo.addEventListener('click', () => {
+        mobileMenu.classList.add('show-mobile')
+    })
+    mobileMenuClose.addEventListener('click', () => {
+        mobileMenu.classList.remove('show-mobile')
+    })
+
+
+
 }
+
 
 eventListeners()
